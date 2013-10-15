383144e5fe94528253eb9d510d32798745f967b6
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-10-15 11:57:11 +0900

    image number 처리 완료

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,14 @@ module ApplicationHelper
     numbers = numbers.gsub(/\<li class\=\"num\"\>\,\<\/li\>/){|s| '<li>,</li>'}
   end
   
-  def number_to_bing_image_tag(number)
-    parts = number.to_s.split(".")
-    parts[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
-    numbers = parts.join
-    numbers = numbers.gsub(/./){|s| '<li class="num">' + s + '</li>'}
-    numbers = numbers.gsub(/\<li class\=\"num\"\>\,\<\/li\>/){|s| '<li>,</li>'}
+  def number_to_big_image_tag(number)
+    parts = number.to_s
+    add_zeros = "0" * (7 - parts.length)
+    parts = parts.insert(0, add_zeros)
+    parts.gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
+    numbers = parts
+    numbers = numbers.gsub(/./){|s| content_tag(:div, image_tag('page_tab/num/' + s + '.jpg'), class: "n")}
+    numbers = numbers.gsub(/\<div class\=\"n\"\>\<img alt\=\"\,\" src\=\"\/images\/page\_tab\/num\/\,\.jpg\" \/\>\<\/div\>/){|s| '<div class="n spacer">&nbsp</div>'}
   end
   
 end
