dcf04877f61c6dc22a4718900370b9e5a074a2eb
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-10-17 20:10:26 +0900

    푸터·포스팅 이미지 수정

--- app/models/wall_post.rb
+++ app/models/wall_post.rb
@@ -27,39 +27,4 @@ class WallPost < ActiveRecord::Base
     end
     Hero.registering(user)
   end
-  
-  # def post
-  #   # api = Koala::Facebook::API.new(self.user.token.access_token)
-  #   # picture = Koala::UploadableIO.new(File.open(Rails.root.to_s+"/app/assets/images/posting_img.jpg"))
-  #   # api.put_connections("me", "namespace.listen", {
-  #   #   # "image[0][url]" => "http://farm4.staticflickr.com/3665/10288523825_3fea0316ef_o.jpg",
-  #   #   # "image[0][user_generated]" => "true",
-  #   #   :article => "Hello, world"})
-  #   
-  #   # Koala::Facebook::API.new(access_token).put_connections(
-  #   #   uid,
-  #   #   "namespace:action",
-  #   #   "image[0][user_generated]" => true,
-  #   #   "image[0][url]" => "[URL_TO_USER_GENERATED_IMAGE]",
-  #   #   "fb:explicitly_shared" => true,
-  #   #   "message" => "[DESCRIPTION]")
-  #   
-  # end
-  
-  # def post
-  #   # dir = Dir.pwd.concat(Rails.root.to_s+"/app/assets/images")
-  #   fb_url = URI.parse("https://graph.facebook.com/#{self.user.uid}/feed")
-  #   img = File.open(Rails.root.to_s+"/app/assets/images/posting_img.jpg")
-  #   req = Net::HTTP::Post::Multipart.new(
-  #     "#{fb_url.path}?access_token=#{self.user.token.access_token}", 
-  #     "source" => UploadIO.new(img, "application/jpg", img.path),
-  #     "message" => self.post_message
-  #   ) 
-  #   n = Net::HTTP.new(fb_url.host, fb_url.port)
-  #   n.use_ssl = true
-  #   n.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  #   n.start do |http|
-  #     @result = http.request(req)
-  #   end
-  # end
 end
