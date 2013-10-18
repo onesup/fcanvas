7458d4384718b3ad52bf45e28ebd8879afb80c04
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-10-18 14:12:02 +0900

    Revert fbf8e80..c378ac6
    This rolls back to commit fbf8e80933a0c704424a6c8986c80d8444e788b5.

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -4,32 +4,17 @@ class Hero < ActiveRecord::Base
   belongs_to :user
   belongs_to :wall_post
   
-  # def self.registerd_heroes_count
-  #   result = 0
-  #   result = Hero.last.heroes_total unless Hero.count == 0
-  #   result
-  # end
-  
   def self.registerd_heroes_count
-    result = 0
-    result = Hero.last.messages_total unless Hero.count == 0
-    result
+    Hero.count == 0 ?  0 : Hero.last.total
   end
-  
   def self.registering(user)
-    total_heroes = Hero.count == 0 ?  0 : Hero.last.heroes_total
-    total_messages = Hero.count == 0 ?  0 : Hero.last.messages_total
+    total_heroes = Hero.count == 0 ?  0 : Hero.last.total
     if user.wall_posts.count == 1
       total_heroes += 1
-      total_messages += 1
-      hero = Hero.create(heroes_total: total_heroes, user: user, messages_total: total_messages)
-      total = [total_heroes, total_messages]
+      Hero.create(total: total_heroes, user: user)
+      total_heroes
     else
-      total_messages += 1
-      hero = Hero.create(heroes_total: total_heroes, user: user, messages_total: total_messages)
-      total = [total_heroes, total_messages]
+      false
     end
-    total
   end
-
 end
--- db/migrate/20131018030318_add_message_total_to_heroes.rb
+++
@@ -1,6 +0,0 @@
-class AddMessageTotalToHeroes < ActiveRecord::Migration
-  def change
-    add_column :heroes, :messages_total, :integer
-    rename_column :heroes, :total, :heroes_total
-  end
-end
