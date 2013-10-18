ef773d5eac03737065a3c6646edf1faa3dfb5d7b
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-10-18 16:07:00 +0900

    토큰을 세션이 아닌 파라미터로 전달

--- app/controllers/m/wall_posts_controller.rb
+++ app/controllers/m/wall_posts_controller.rb
@@ -1,26 +1,19 @@
 class M::WallPostsController < ApplicationController
   def create
-    unless session[:facebook_uid]
-      redirect_to root_path
-    else
-      user = User.find_by_uid(session[:facebook_uid])
-      wall_post = user.wall_posts.new(wall_post_params)
-      last_post_time = user.wall_posts.empty? ? (Time.now - 7.day) : user.wall_posts.last.created_at
-      respond_to do |format|
-        # if (Time.now - last_post_time) > 20.second
-          # flash[:popup] = "timelimit"
-          # format.html { redirect_to mobile_path}
-        # elsif wall_post.save
-        if wall_post.save
-          wall_post.post
-          flash[:popup] = "complete"
-          format.html { redirect_to mobile_path}
-          format.json { render json: wall_post, status: :created, location: wall_post }
-        else
-          flash[:popup] = "invalid"
-          format.html { redirect_to mobile_path }
-          format.json { render json: wall_post.errors, status: :unprocessable_entity }
-        end
+    uid = params[:uid]
+    access_token = params[:access_token]
+    user = User.find_by_uid(uid)
+    wall_post = user.wall_posts.new(wall_post_params)
+    respond_to do |format|
+      if wall_post.save
+        wall_post.post
+        flash[:popup] = "complete"
+        format.html { redirect_to mobile_path}
+        format.json { render json: wall_post, status: :created, location: wall_post }
+      else
+        flash[:popup] = "invalid"
+        format.html { redirect_to mobile_path }
+        format.json { render json: wall_post.errors, status: :unprocessable_entity }
       end
     end
   end
