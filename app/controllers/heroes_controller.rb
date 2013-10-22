d8e79358ad2c84ad3839fde47705ce31f7b92657
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-10-22 14:44:21 +0900

    cors 관련 헤더 추가

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,5 +1,13 @@
 class HeroesController < ApplicationController
+
+  after_filter :set_access_control_headers
+  
   def count
     @count = Hero.registerd_heroes_count
+  end
+  
+  def set_access_control_headers
+     headers['Access-Control-Allow-Origin'] = '*'
+     headers['Access-Control-Request-Method'] = '*'
   end
 end
