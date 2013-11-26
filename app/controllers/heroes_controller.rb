fb93e897a3fef6046e18f6f0b01f83edeaf76e52
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-11-26 11:19:35 +0900

    api for 푸르메재단

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,6 +1,6 @@
 class HeroesController < ApplicationController
-  layout false
-  after_filter :set_access_control_headers
+  # layout false
+  # after_filter :set_access_control_headers
   
   def count
     @count = Hero.registerd_heroes_count
@@ -9,6 +9,11 @@ class HeroesController < ApplicationController
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
+  end
+  
+  def counter
+    @count = Hero.registerd_heroes_count
+    
   end
   
   def render_test
