c0b339387339c00361e825b1cf4c793d42ee1ab9
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-12-17 18:01:46 +0900

    relay_config

--- config/initializers/facebook_config.rb
+++ config/initializers/facebook_config.rb
@@ -1,1 +1,8 @@
 FACEBOOK_CONFIG = YAML.load_file("#{Rails.root}/config/facebook.yml")[Rails.env]
+RELAY_CONFIG = {
+  :page_id=>"1377173915843709", 
+  :app_id=>"666292156743698", 
+  :app_secret=>"53a9b2e4f6781ad1852290626c0a91bb", 
+  :domain => "ssl.minivertising.kr", 
+  :page_address => "https://www.facebook.com/pages/ThchStartup/1377173915843709?id=1377173915843709&sk=app_166052670263191"
+}
