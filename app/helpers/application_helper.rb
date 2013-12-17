0a37657eddbe8a7bae4d690fbf81e1d560e39533
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-12-17 22:00:11 +0900

    Revert "relay camaign 추가"
    This reverts commit cc6132298de023ec6c174f43e4e940491014dd13.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -81,54 +81,42 @@ module ApplicationHelper
     numbers = numbers.gsub(/\<div class\=\"n\"\>\<img alt\=\"\,\" src\=\"\/images\/page\_tab\/num\/\,\.jpg\" \/\>\<\/div\>/){|s| '<div class="n spacer">&nbsp</div>'}
   end
   
-  # def fans_to_boxes(heros)
-  #   donate_unit_won = 5000
-  #   stage1 = 10000000
-  #   stage2 = 30000000
-  #   stage3 = 50000000
-  #   box_value = stage1 / 20
-  #   stage1_boxes = stage1 / box_value
-  #   stage2_boxes = (stage2 / box_value) - stage1_boxes
-  #   stage3_boxes = (stage3 / box_value) - (stage1_boxes + stage2_boxes)
-  #   total_boxes = stage1_boxes + stage2_boxes + stage3_boxes
-  #   donated_boxes = (heros * donate_unit_won) / box_value
-  #   stage1_result = stage1_boxes - donated_boxes
-  #   total_stock = total_boxes - donated_boxes
-  #   stage1_result = stage1_boxes 
-  #   stage2_result = stage2_boxes
-  #   stage3_result = stage3_boxes
-  #   stage1_stock = total_stock - (stage2_boxes + stage3_boxes) > 0 ? total_stock - (stage2_boxes + stage3_boxes) : 0
-  #   if (stage1_boxes + stage3_boxes) - donated_boxes > 0
-  #     stage2_stock = (stage1_boxes + stage3_boxes) - donated_boxes
-  #   elsif (stage1_boxes + stage3_boxes) - donated_boxes > stage2_boxes
-  #   else
-  #     stage2_stock = 0
-  #   end
-  #   if donated_boxes > (stage1_boxes + stage2_boxes)
-  #     stage3_stock = total_stock
-  #   else
-  #     stage3_stock = stage3_boxes
-  #   end
-  #   [[stage1_boxes, stage1_stock], [stage2_boxes, stage2_stock], [stage3_boxes, stage3_stock]]
-  # end
+  def number_to_big_image_tag_for_org(number)
+    # http://www.miraclehospital.org/img/common/img/sns_event_num6.gif
+    parts = number.to_s
+    add_zeros = "0" * (7 - parts.length)
+    parts = parts.insert(0, add_zeros)
+    parts.gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
+    numbers = parts
+<<<<<<< HEAD
+    numbers = numbers.gsub(/./){|s| "<div class='n'><img alt='"+s+"' src='img/common/img/sns_event_num" + s + ".jpg' /></div>"}
+    numbers = numbers.gsub(/\<div class\=\'n\'\>\<img alt\=\'\,\' src\=\'img\/common\/img\/sns_event_num\,\.jpg\' \/\>\<\/div\>/){|s| "<div class='n spacer'>&nbsp</div>"}
+  end
+=======
+    numbers = numbers.gsub(/./){|s| content_tag(:div, raw('<img alt="'+s+'" src="img/common/img/sns_event_num' + s + '.jpg" />'), class: "n")}
+    numbers = numbers.gsub(/\<div class\=\"n\"\>\<img alt\=\"\,\" src\=\"img\/common\/img\/sns_event_num\,\.jpg\" \/\>\<\/div\>/){|s| '<div class="n spacer">&nbsp</div>'}
+  end
+  
+>>>>>>> parent of ad6c1c6... Revert "api for 푸르메재단"
+
   
   def render_stage1(heros)
     stocks = fans_to_boxes(heros)[0]
     boxes = stocks.first
     stocks = stocks.last
     if stocks == 20
-      blanks = '<li class="blank"></li>' * stocks
+      blanks = "<li class='blank'></li>" * stocks
       result = blanks
     elsif stocks < 20 and stocks >= 10
-      blanks2 = '<li class="blank"></li>' * (stocks - 10)
-      fills1 = '<li class="fill_01"></li>' * (boxes - stocks)
-      blanks1 = '<li class="blank"></li>' * 10
+      blanks2 = "<li class='blank'></li>" * (stocks - 10)
+      fills1 = "<li class='fill_01'></li>" * (boxes - stocks)
+      blanks1 = "<li class='blank'></li>" * 10
       result = blanks1 + fills1 + blanks2
     elsif stocks < 9
       # blanks1 = '<li class="blank"></li>' * (10)
-      blanks1 = '<li class="blank"></li>' * (stocks)
-      fills1 = '<li class="fill_01"></li>' * ((boxes - stocks) - 10)
-      fills2 = '<li class="fill_01"></li>' * 10 
+      blanks1 = "<li class='blank'></li>" * (stocks)
+      fills1 = "<li class='fill_01'></li>" * ((boxes - stocks) - 10)
+      fills2 = "<li class='fill_01'></li>" * 10 
       result = fills1 + blanks1 + fills2
     end
     result
@@ -142,7 +130,7 @@ module ApplicationHelper
     result = String.new
     init_line.each do |box|
       if box.nil?
-        result << '<li class="blank"></li>'
+        result << "<li class='blank'></li>"
       else
         result << '<li class="fill_01"></li>'
       end
---
+++ app/views/heroes/counter.json.jbuilder
@@ -0,0 +1,4 @@
+json.counter number_to_big_image_tag_for_org(Hero.registerd_heroes_count)
+json.box_group_1 render_a_box_group("first",box_set(@count)[0])
+json.box_group_2 render_a_box_group("first",box_set(@count)[1])
+json.box_group_3 render_a_box_group("first",box_set(@count)[2])
