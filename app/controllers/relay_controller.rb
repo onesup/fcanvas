2c57355ce3c03a78962906ed8bb9a75af6114059
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-01-17 16:13:39 +0900

    koala signed_request

--- app/controllers/relay_controller.rb
+++ app/controllers/relay_controller.rb
@@ -14,8 +14,12 @@ class RelayController < ApplicationController
     app_secret = FACEBOOK_CONFIG[:relay][:app_secret]
     oauth = Koala::Facebook::OAuth.new(app_id, app_secret)
     Rails.logger.info("@@@@@@@" + app_id + "@@@@@@" + app_secret)
-    # request = oauth.parse_signed_request(params["signed_request"])
-    # Rails.logger.info("@@@@@@@" + request["page"]["liked"].to_s)
+    begin
+      request = oauth.parse_signed_request(params["signed_request"])
+    rescue
+      request["page"]["liked"] = "@@@@@@invalid!!!!"
+    end
+    Rails.logger.info("@@@@@@@" + request["page"]["liked"].to_s)
   end
   
   def mobile_relay
