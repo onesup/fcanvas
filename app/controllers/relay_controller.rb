59bdafac8f957c246caecf10774408a38f8aa8b8
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-01-17 15:59:23 +0900

    디버깅

--- app/controllers/relay_controller.rb
+++ app/controllers/relay_controller.rb
@@ -13,8 +13,9 @@ class RelayController < ApplicationController
     app_id = FACEBOOK_CONFIG[:relay][:app_id]
     app_secret = FACEBOOK_CONFIG[:relay][:app_secret]
     oauth = Koala::Facebook::OAuth.new(app_id, app_secret)
-    request = oauth.parse_signed_request(params["signed_request"])
-    Rails.logger.info("@@@@@@@" + request["page"]["liked"].to_s)
+    Rails.logger.info("@@@@@@@" + app_id + "@@@@@@" + app_secret)
+    # request = oauth.parse_signed_request(params["signed_request"])
+    # Rails.logger.info("@@@@@@@" + request["page"]["liked"].to_s)
   end
   
   def mobile_relay
