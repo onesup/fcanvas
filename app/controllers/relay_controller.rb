eed08778d242bd64767c959c214804b1d4de34d7
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-01-17 15:49:57 +0900

    좋아요 여부 조사

--- app/controllers/relay_controller.rb
+++ app/controllers/relay_controller.rb
@@ -10,7 +10,11 @@ class RelayController < ApplicationController
   end
   
   def tab_relay
-    
+    app_id = FACEBOOK_CONFIG[:relay][:app_id]
+    app_secret = FACEBOOK_CONFIG[:relay][:app_secret]
+    oauth = Koala::Facebook::OAuth.new(app_id, app_secret)
+    request = oauth.parse_signed_request(params["signed_request"])
+    Rails.logger.info("@@@@@@@" + request["page"]["liked"].to_s)
   end
   
   def mobile_relay
