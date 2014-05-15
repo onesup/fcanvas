db362ddb498bacbeb50ac5dada41368d3320ef8a
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-05-15 14:25:06 +0900

    unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,27 +1,8 @@
-require "tmpdir"
-TMP_DIR = Dir.tmpdir
-USER_NAME = "onesup"
-APP_NAME = "fcanvas"
-APP_PATH = File.expand_path("/home/#{USER_NAME}/www/#{APP_NAME}/current")
-
-root = "#{APP_PATH}"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.err.log"
-stdout_path "#{root}/log/unicorn.out.log"
- 
-# change the YOUR_APP_NAME to your application name
-listen "#{TMP_DIR}/.unicorn.#{APP_NAME}.sock", :backlog => 512
-# listen "/tmp/unicorn.coffee_letter.sock"
-worker_processes 2
-timeout 180
-preload_app false
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
-end
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 
 before_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -32,9 +13,8 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
