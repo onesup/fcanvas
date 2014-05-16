de243a717ec176dc57b78b8f08df29d2820811ef
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-05-16 17:11:09 +0900

    이벤트 종료 안내.

--- app/controllers/page_tab_controller.rb
+++ app/controllers/page_tab_controller.rb
@@ -1,7 +1,7 @@
 class PageTabController < ApplicationController
   def index
     session[:facebook_params] = request.env['facebook.params']
-    require_like unless session[:facebook_params].nil?
+    # require_like unless session[:facebook_params].nil?
     @wall_post = WallPost.new
     @wall_posts = WallPost.order('created_at DESC').page(params[:page]).per(5)
     @count = WallPost.count
@@ -18,18 +18,18 @@ class PageTabController < ApplicationController
   private
 
     def require_like
-      facebook_params = session[:facebook_params]
-      if facebook_params['page']['liked'] == false
-        redirect_to page_tab_gate_path
-      else
-        begin
-          user = User.create_or_find_fan!(facebook_params['user_id'], facebook_params['oauth_token'])
-          session[:facebook_uid] = user.uid
-          session[:facebook_token] = user.token.access_token
-        rescue
-          redirect_to page_tab_path
-        end
-      end
+      # facebook_params = session[:facebook_params]
+      # if facebook_params['page']['liked'] == false
+      #   redirect_to page_tab_gate_path
+      # else
+      #   begin
+      #     user = User.create_or_find_fan!(facebook_params['user_id'], facebook_params['oauth_token'])
+      #     session[:facebook_uid] = user.uid
+      #     session[:facebook_token] = user.token.access_token
+      #   rescue
+      #     redirect_to page_tab_path
+      #   end
+      # end
 
     end
   
